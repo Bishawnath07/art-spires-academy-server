889188841b4960f799013c1fd97d255817738c40
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-06-07 23:35:47 +0600

    user admin method added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
  app.use(express.json());
  
  
- const { MongoClient, ServerApiVersion } = require('mongodb');
+ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eo0io7y.mongodb.net/?retryWrites=true&w=majority`
 
@@ -29,6 +29,41 @@ async function run() {
 
     const classCollection = client.db("artspiresDB").collection("classes")
     const instructorsCollection = client.db("artspiresDB").collection("instructors")
+    const usersCollection = client.db("artspiresDB").collection("users")
+
+
+    // users relatate api
+    app.get('/users' , async (req , res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users' , async(req , res) =>{
+        const user = req.body;
+        console.log(user)
+        const query = {email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+        if(existingUser){
+          return res.send({message: 'user already exists'})
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      })
+
+       app.patch('/users/admin/:id' , async(req ,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)} ;
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      }
+      const result = await usersCollection.updateOne(filter , updateDoc)
+      res.send(result)
+    })
+
+
+
 
 
     // classes relate api
